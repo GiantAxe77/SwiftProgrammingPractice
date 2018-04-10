23cc28133a367d96b35b84fdfb068ae1aab04c57
guowei <giantaxe77@163.com>
2018-04-10 17:55:29 +0800

    ControlFlow练习

--- Practice/Practice/ControlFlow.playground/Contents.swift
+++ Practice/Practice/ControlFlow.playground/Contents.swift
@@ -9,3 +9,69 @@ for index in 1...5 {
     print(_: "\(index)", terminator: "")
 }
     
+
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+var naturalCount: String
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+default:
+    naturalCount = "many"
+}
+print("There are \(naturalCount) \(countedThings).")
+
+
+let somePoint = (1, 1)
+switch somePoint {
+case (0, 0):
+    print("0,0")
+case (_, 0):
+    print("x-axis")
+case (0, _):
+    print("y-axis")
+default:
+    print("outside point")
+}
+
+
+let anotherPoint = (1, -1)
+switch anotherPoint {
+case let (x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+
+
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("not on an axis")
+}
+
+
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+for character in puzzleInput.characters {
+    switch character {
+    case "a", "e", "i", "o", "u", " ":
+        continue
+    default:
+        puzzleOutput.append(character)
+    }
+}
+print(puzzleOutput)
+
