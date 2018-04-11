15555837640f9e89195abfd84373e88b4302b3c7
GiantAxe <giantaxe77@163.com>
2018-04-11 22:02:34 +0800

    提交控制流练习

--- Practice/Practice/ControlFlow.playground/Contents.swift
+++ Practice/Practice/ControlFlow.playground/Contents.swift
@@ -8,7 +8,7 @@ var str = "Hello, playground"
 for index in 1...5 {
     print(_: "\(index)", terminator: "")
 }
-    
+
 
 let approximateCount = 62
 let countedThings = "moons orbiting Saturn"
@@ -74,4 +74,60 @@ for character in puzzleInput.characters {
     }
 }
 print(puzzleOutput)
+
+
+let numberSymbol: Character = "三" // 简体中文里的数字 3
+var possibleIntegerValue: Int?
+switch numberSymbol {
+case "1", "?", "一", "?":
+    possibleIntegerValue = 1
+case "2", "?", "二", "?":
+    possibleIntegerValue = 2
+case "3", "?", "三", "?":
+    possibleIntegerValue = 3
+case "4", "?", "四", "?":
+    possibleIntegerValue = 4
+default:
+    break
+}
+if let integerValue = possibleIntegerValue {
+    print("The integer value of \(numberSymbol) is \(integerValue).")
+} else {
+    print("An integer value could not be found for \(numberSymbol).")
+}
+
+
+let integerToDescribe = 5
+var desc = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2,3,5,7:
+    desc += "a prime number, and also "
+    fallthrough
+default:
+    desc += "an integer"
+}
+print(desc)
+
+
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    print("Hello, \(name)")
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    print("I hope the weather is nice in \(location).")
+}
+greet(person: ["name": "John"])
+greet(person: ["name": "Jane", "location": "Cupertino"])
+
+
+if #available(iOS 10, macOS 10.12, *) {
+    print("iOS 10, macOS 10.12")
+} else {
+    print("使用先前版本的 iOS 和 macOS 的 API")
+}
+
 
