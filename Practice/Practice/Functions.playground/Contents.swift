c6408d2291112f0455acf53686974a43106e9589
GiantAxe <giantaxe77@163.com>
2018-04-14 23:18:10 +0800

    Functions练习

--- Practice/Practice/Functions.playground/Contents.swift
+++ Practice/Practice/Functions.playground/Contents.swift
@@ -95,3 +95,39 @@ swap(&someInt, &anotherInt)
 print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
 
 
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+var mathFunction: (Int, Int) -> Int = addTwoInts
+print(mathFunction(2,3))
+
+
+// 函数类型作为参数类型
+func printMathResult(_ mathFunction2:(Int, Int) -> Int, _ a: Int, _ b:Int) {
+    print(mathFunction2(a, b))
+}
+printMathResult(addTwoInts, 3, 5)
+
+
+// 函数类型作为返回类型
+func stepForward(_ input: Int) -> Int {
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    return input - 1
+}
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    return backward ? stepBackward : stepForward
+}
+var currentValue = 3
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+
+print("Counting to zero:")
+while currentValue != 0 {
+    print("\(currentValue)... ") 
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!!!")
+
+
+
