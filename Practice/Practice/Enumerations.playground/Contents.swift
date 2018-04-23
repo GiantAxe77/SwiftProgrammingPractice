324ec63ddad321b617ee7d4c2424196bb08b7704
GiantAxe <giantaxe77@163.com>
2018-04-23 23:23:42 +0800

    枚举练习

--- Practice/Practice/Enumerations.playground/Contents.swift
+++ Practice/Practice/Enumerations.playground/Contents.swift
@@ -4,3 +4,38 @@ import Cocoa
 
 var str = "Hello, playground"
 
+enum SomeEnumeration {
+    // 枚举定义放在这里
+}
+
+
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+
+enum Planet {
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+
+var directionToHead = CompassPoint.west
+directionToHead = .north
+
+// 使用 Switch 语句匹配枚举值
+switch directionToHead {
+case .north:
+    print("north")
+case .west:
+    print("west")
+case .east:
+    print("west")
+default:
+    print("default")
+}
+
+
+
